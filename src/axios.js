664f937dc2a100e2f2c16f5cd6b93fda3c80a047
Prateek <prateeksdr4@gmail.com>
2021-06-23 02:28:32 +0530

    modified gitignore file

--- src/axios.js
+++ src/axios.js
@@ -4,4 +4,5 @@ const instance = axios.create({
     baseURL: "https://us-central1-clone-b8ccc.cloudfunctions.net/api"
 });
 export default instance;
+
 // 'http://localhost:5001/clone-b8ccc/us-central1/api'
